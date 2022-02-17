8d1403ed5dde3903506088dc2f3e7711a6a8687c
kykim <kykim@euclidsoft.co.kr>
2022-02-17 09:38:45 +0900

    tailwindcss 샘플 첫 커밋

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'logo-color' : '#EF4B81',
+      
+    },
+    fontFamily: {
+      dongleb : ['Dongle-Bold'],
+      donglel : ['Dongle-Light'],
+      dongler : ['Dongle-Regular'],
+    },
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
